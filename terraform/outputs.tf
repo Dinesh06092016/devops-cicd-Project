3eeec19a310321270367a4d96f761ebadf31fb1c
Dinesh06092016 <dhee.kumar84@gmail.com>
2025-09-27 21:53:21 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
 output "master_ip" {
-  value = aws_instance.master_node.65.0.93.6
+  value = aws_instance.master_node.public_ip
 }
 
 output "worker1_ip" {
-  value = aws_instance.worker_nodes[0].13.232.106.40
+  value = aws_instance.worker_nodes[0].public_ip
 }
 
 output "worker2_ip" {
-  value = aws_instance.worker_nodes[1].3.109.152.183
+  value = aws_instance.worker_nodes[1].public_ip
 }
