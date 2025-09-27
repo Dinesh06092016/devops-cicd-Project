82b78b042900db57323f6994e611a35c926def25
Dinesh06092016 <dhee.kumar84@gmail.com>
2025-09-27 22:39:47 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,22 @@
 output "k8s_master_ip" {
-  value = aws_instance.k8s_master_node.public_ip
+  description = "Public IP address of the Kubernetes master node"
+  value       = aws_instance.k8s_master.public_ip
 }
 
-output "k8s_worker_ip" {
-  value = aws_instance.worker_nodes[0].public_ip
+output "k8s_worker_ips" {
+  description = "Public IP addresses of Kubernetes worker nodes"
+  value       = aws_instance.k8s_worker[*].public_ip
+}
+
+output "k8s_master_dns" {
+  description = "Public DNS of the Kubernetes master node"
+  value       = aws_instance.k8s_master.public_dns
+}
+
+output "ssh_commands" {
+  description = "SSH commands to connect to all nodes"
+  value = {
+    master = "ssh -i ~/.ssh/22nd-Sep.pem ubuntu@${aws_instance.k8s_master.public_dns}"
+    workers = [for i, worker in aws_instance.k8s_worker : "ssh -i ~/.ssh/22nd-Sep.pem ubuntu@${worker.public_dns}"]
+  }
 }
