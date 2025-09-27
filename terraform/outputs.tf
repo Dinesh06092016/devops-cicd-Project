226bc57e6477cebe171c2a239b625d4688a7f24a
Dinesh06092016 <dhee.kumar84@gmail.com>
2025-09-27 22:02:59 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,7 @@
-output "master_ip" {
-  value = aws_instance.master_node.public_ip
+output "k8s_master_ip" {
+  value = aws_instance.k8s_master_node.public_ip
 }
 
-output "worker1_ip" {
+output "k8s_worker_ip" {
   value = aws_instance.worker_nodes[0].public_ip
-}
-
-output "worker2_ip" {
-  value = aws_instance.worker_nodes[1].public_ip
 }
