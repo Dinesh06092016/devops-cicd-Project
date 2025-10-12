40f04075fe76a2134d58c1851a64a1d19f72f444
Dinesh06092016 <dhee.kumar84@gmail.com>
2025-10-12 16:45:42 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,34 +1,17 @@
-output "instance_ips" {
-  value = {
-    master  = aws_instance.k8s_master.public_ip
-    workers = [for w in aws_instance.k8s_worker : w.public_ip]
-  }
-}
-
-output "k8s_instance_ips" {
-  value = {
-    master  = aws_instance.k8s_master.public_ip
-    workers = [for w in aws_instance.k8s_worker : w.public_ip]
-  }
-}
-
-output "k8s_master_dns" {
-  value = aws_instance.k8s_master.public_dns
-}
-
+# Output the Kubernetes master public IP
 output "k8s_master_ip" {
   value = aws_instance.k8s_master.public_ip
 }
 
+# Output the Kubernetes worker public IPs
 output "k8s_worker_ips" {
   value = [for w in aws_instance.k8s_worker : w.public_ip]
 }
 
+# Output SSH command examples to connect to master and workers
 output "k8s_ssh_commands" {
   value = {
-    master = "ssh -i ~/.ssh/22nd-sep.pem ubuntu@${aws_instance.k8s_master.public_dns}"
-    workers = [
-      for w in aws_instance.k8s_worker : "ssh -i ~/.ssh/22nd-sep.pem ubuntu@${w.public_dns}"
-    ]
+    master  = "ssh -i ~/.ssh/22nd-Sep.pem ubuntu@${aws_instance.k8s_master.public_ip}"
+    workers = [for w in aws_instance.k8s_worker : "ssh -i ~/.ssh/22nd-Sep.pem ubuntu@${w.public_ip}"]
   }
 }
