e1409509210df778ec82e689d5bfe8ab9c4a55eb
Dinesh06092016 <dhee.kumar84@gmail.com>
2025-10-12 14:42:16 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,23 +1,27 @@
-output "k8s_master_ip" {
-  description = "Public IP address of the Kubernetes master node"
-  value       = aws_instance.k8s_master.public_ip
+output "instance_ips" {
+  value = {
+    master  = "3.111.32.154"
+    workers = ["13.232.255.82"]
+  }
 }
 
-output "k8s_worker_ips" {
-  description = "Public IP addresses of Kubernetes worker nodes"
-  value       = aws_instance.k8s_worker[*].public_ip
+output "k8s_master_ip" {
+  value = "3.111.32.154"
 }
 
 output "k8s_master_dns" {
-  description = "Public DNS of the Kubernetes master node"
-  value       = aws_instance.k8s_master.public_dns
+  value = "ec2-3-111-32-154.ap-south-1.compute.amazonaws.com"
+}
+
+output "k8s_worker_ips" {
+  value = ["13.232.255.82"]
 }
 
 output "ssh_commands" {
-  description = "SSH commands to connect to all nodes"
   value = {
-    master = "ssh -i ~/.ssh/22nd-Sep.pem ubuntu@ec2-3-111-32-154.ap-south-1.compute.amazonaws.com
-}"
-    workers = [for i, worker in aws_instance.k8s_worker : "ssh -i ~/.ssh/22nd-Sep.pem ubuntu@${worker.public_dns}"]
+    master  = "ssh -i ~/.ssh/22nd-Sep.pem ubuntu@ec2-3-111-32-154.ap-south-1.compute.amazonaws.com"
+    workers = [
+      "ssh -i ~/.ssh/22nd-Sep.pem ubuntu@ec2-13-232-255-82.ap-south-1.compute.amazonaws.com"
+    ]
   }
 }
