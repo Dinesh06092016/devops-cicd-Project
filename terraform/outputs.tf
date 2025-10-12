1410cf6c9e43ce4529ed0f1723ebd9975d2c1675
Dinesh06092016 <dhee.kumar84@gmail.com>
2025-10-12 14:38:40 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -16,7 +16,8 @@ output "k8s_master_dns" {
 output "ssh_commands" {
   description = "SSH commands to connect to all nodes"
   value = {
-    master = "ssh -i ~/.ssh/22nd-Sep.pem ubuntu@${aws_instance.k8s_master.public_dns}"
+    master = "ssh -i ~/.ssh/22nd-Sep.pem ubuntu@ec2-3-111-32-154.ap-south-1.compute.amazonaws.com
+}"
     workers = [for i, worker in aws_instance.k8s_worker : "ssh -i ~/.ssh/22nd-Sep.pem ubuntu@${worker.public_dns}"]
   }
 }
