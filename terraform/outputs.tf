753044815c2baec3e4699d165439efd5392c19a8
Dinesh06092016 <dhee.kumar84@gmail.com>
2025-10-12 14:46:18 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,23 +1,32 @@
-output "instance_ips" {
+# outputs.tf
+
+# Output the master and worker instance IPs
+output "k8s_instance_ips" {
   value = {
     master  = "3.111.32.154"
     workers = ["13.232.255.82"]
   }
 }
 
-output "k8s_master_ip" {
-  value = "3.111.32.154"
-}
-
+# Output the DNS of the master node
 output "k8s_master_dns" {
   value = "ec2-3-111-32-154.ap-south-1.compute.amazonaws.com"
 }
 
-output "k8s_worker_ips" {
-  value = ["13.232.255.82"]
+# Output the master IP
+output "k8s_master_ip" {
+  value = "3.111.32.154"
 }
 
-output "ssh_commands" {
+# Output the worker IPs
+output "k8s_worker_ips" {
+  value = [
+    "13.232.255.82"
+  ]
+}
+
+# Output SSH commands to access nodes
+output "k8s_ssh_commands" {
   value = {
     master  = "ssh -i ~/.ssh/22nd-Sep.pem ubuntu@ec2-3-111-32-154.ap-south-1.compute.amazonaws.com"
     workers = [
